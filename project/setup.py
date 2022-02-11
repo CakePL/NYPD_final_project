19b6391519b010fb8d3d7e08cd1679244d31112f
CakePL <mateusz.borun@onet.pl>
2022-02-11 13:39:39 +0100

    added setup.py, moved profile

---
+++ project/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="pit_analysis",
+    version="0.0.1",
+    author="Mateusz Borun",
+    author_email="m.borun@student.uw.edu.pl",
+    description="Final NYPD project",
+    url="https://github.com/CakePL/NYPD_final_project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
